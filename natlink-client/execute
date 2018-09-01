96bcb0f385c9fdee3b7cb107b6aa02efe64f77b3
Spiros Boosalis <SamBoosalis@gmail.com>
2018-09-01 15:11:02 -0700

    `_dictation.py` works

---
+++ natlink-client/execute
@@ -0,0 +1,2 @@
+#!/bin/bash
+python ${1:?[Usage] "$0" FILE.py}
--- natlink-client/sources/_dictation.py
+++ natlink-client/sources/_dictation.py
@@ -3,14 +3,22 @@
 # notes
 # =====
 #
-# this is an example (natlink-)plugin
-# of a simple "Dictation" grammar.
+# this is a (natlink-)plugin for a simple "Dictation" grammar.
+#
+# the grammar has only:
+# - `<dgndictation>` (i.e. arbitrary dictation); and
+# - `<dgnletters> (i.e. spelling letters and numbers)
+#
+# the client supports only "forwarding" the recognitions to a server.
+# it is simple and stateless; there are no other features,
+# like corrections (`ResObj.correction()`),
+# or microphone toggling (`setMicState()`).
 # 
 # the name of this file is prefixed with an underscore;
 # this tells Dragon that the grammar is "global"
 # (i.e. active independent of the current active window).
 #
-# 
+
 ##################################################
 # (standard-library modules)
 
@@ -20,8 +28,10 @@ import os
 ##################################################
 # (project (local) modules)
 
-from   sboo.types   import *
-from   sboo.grammar import *
+#from   sboo.utilities import *
+from   sboo.api       import *
+from   sboo.grammar   import *
+from   sboo.types     import *
 
 ##################################################
 # (natlink13 modules)
@@ -32,9 +42,21 @@ if os.environ.get('SBOO_NATLINK'):
     # ^ for testing on the host, import stubbed `natlink` signatures.
 
 else:
-    # tyhe environment-variable is unset or set to a falsy value.
+    # the environment-variable is unset or set to a falsy value.
     from      natlinkmain import (setCheckForGrammarChanges)
     # ^ for running on the guest.
+
+if os.environ.get('SBOO_NATLINK'):
+    import sboo.gramparser as gramparser
+
+else:
+    import                    gramparser
+
+# ^ `gramparser`:
+#
+#      This module contains the Python code to convert the textual represenation
+#   of a command and control grammar in the standard SAPI CFG binary format.
+#
 
 ##################################################
 # The User's Grammar
@@ -45,7 +67,13 @@ rules = '''
 <dgndictation> imported;
 <dgnletters>   imported;
 
+<dictating> exported = [ say ] <dgndictation>;
+<spelling>  exported = spell   <dgnletters>;
+
+#TODO# <inserting> exported = send [ <insertion_method> ] <dgndictation>;
+#TODO# <insertion_method> = text | keys | clip | paste;
 '''
+
 lists = { }
 
 ##################################################
@@ -54,10 +82,19 @@ config = GrammarConfig(exports=exports,
                        rules=rules,
                        lists=lists)
 
-props = fastProperties
+dictationProperties = Properties( activity               = Activity.ACTIVE,
+                                 exclusivity            = Exclusivity.INCLUSIVE,
+                                 shouldEavesdrop        = True,
+                                 shouldHypothesize      = True,
+                                 doOnlyGotResultsObject = True)
+
+properties = dictationProperties
+#properties = generousProperties
 
 ##################################################
 # The Server's Address
+
+address = defaultAddress
 
 ##################################################
 # The boilerplate that `natlink` requires of a `natlink` plugin.
@@ -70,8 +107,8 @@ def load():
 
     setCheckForGrammarChanges(1)
     # ^ automatically reload on file change
-    # (versus only when the microphone toggles on,
-    # the default behavior).
+    # (versus only when the microphone toggles back on,
+    # which is the default behavior).
     
     GRAMMAR = NarcissisticGrammar()
     GRAMMAR.initialize(config, properties=defaultProperties)
@@ -85,4 +122,49 @@ def unload():
     GRAMMAR = None
 
 load()
+
+##################################################
+
+
+print '--------------------------------------------------'
+print '[source grammar]'
+print
+print rules
+print 
+print '--------------------------------------------------'
+print '[binary grammar]'
+print 
+
+linedRules = rules.split('\n')
+parser = gramparser.GramParser(linedRules)
+parser.doParse()
+parser.checkForErrors()
+
+binaryRules = parser.dumpString()
+
+print binaryRules
+print 
+print '--------------------------------------------------'
+print '[grammar EXPORTS]'
+print
+print exports
+print 
+print '--------------------------------------------------'
+print '[grammar LISTS]'
+print
+print lists
+print 
+print '--------------------------------------------------'
+print '[grammar PROPERTIES]'
+print
+print properties
+print 
+print '--------------------------------------------------'
+print '[server ADDRESS]'
+print
+print address
+print 
+print '--------------------------------------------------'
+print '--------------------------------------------------'
+
 ##################################################
