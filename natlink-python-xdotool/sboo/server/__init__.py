be12f617376440ddfa56c8efac6c0aba5201f7cf
Spiros Boosalis <SamBoosalis@gmail.com>
2018-09-03 22:07:54 -0700

    `.server`/`.client` work

--- natlink-python-xdotool/sboo/server/__init__.py
+++ natlink-python-xdotool/sboo/server/__init__.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 ##################################################
 
-from .server import __main__
+from .server import main
 
 ##################################################
+
+# [Problem]
+#
+# $ python -m sboo.server            
+# python: No module named sboo.server.__main__; 'sboo.server' is a package and cannot be directly executed
+#
